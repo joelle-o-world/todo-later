387c864e3780215da17541a3ca20c2d72265c4d2
Joelle Plow <joelleplow@aol.com>
2022-01-23 18:37:19 +0000

    feat: Persistant local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,24 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+} from "@reduxjs/toolkit";
 import tasksReducer from "../features/tasks/tasksSlice";
 import settingsReducer from "../features/settings/settingsSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    settings: settingsReducer,
-  },
+  reducer: persistReducer(
+    { key: "root", storage },
+    combineReducers({
+      tasks: tasksReducer,
+      settings: settingsReducer,
+    })
+  ),
 });
+persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
